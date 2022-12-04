bafa4749dc782d3020d6aced6e46164eb29c6620
TestAk123 <ilya_i_2@mail.ru>
2022-12-04 20:44:40 +0700

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,11 +1,44 @@
 ﻿#include <iostream>
+#include <math.h>
 using namespace std;
-double my_pow(double number, unsigned int power)
+
+double my_pow_fast_test( double number, const unsigned int &power, unsigned int power_this, unsigned int &power_present)
+{
+    number *= number;
+    power_this *= 2;
+    power_present = power_this;
+    if (power_this *2 <= power)
+    {
+        double number_returned = my_pow_fast_test(number, power, power_this, power_present);
+        if (power_present + power_this < power)
+        {
+            number *= number_returned;
+            power_present += power_this;
+        }
+        else
+        {
+            return number_returned;
+        }
+    }
+    return number;
+}
+double my_pow_fast_start(double number, unsigned int power)
 {
     double numpow = 1;
-    for (int i = 0; i < power; i++)
+    if (power < 4)
     {
-        numpow *= number;
+        for (int i = 0; i < power; i++)
+        {
+            numpow *= number;
+        }
+        return numpow;
+    }
+    numpow *= number;
+    unsigned int power_present=1;
+    numpow=my_pow_fast_test(numpow, power, power_present, power_present);
+    if (power%2!=0)
+    {
+        numpow*= number;
     }
     return numpow;
 }
@@ -14,11 +47,11 @@ int main()
     setlocale(LC_ALL, "russian");
     double number;
     unsigned int power;
-    cout << "Введите дробное число" << endl;
+    cout << "Введите дробное число" << endl; 
     cin >> number;
     cout << "Введите положительную целую степень" << endl;
     cin >> power;
-    double answer = my_pow(number, power);
+    double answer=my_pow_fast_start(number, power);
     cout << "Число: " << number << endl;
     cout << "Степень: " << power << endl;
     cout << "Число^(Степень): " << answer << endl;
