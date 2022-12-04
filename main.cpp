0f18c5a29d88ff2c2c92a899e12cbbb75c1cafb6
TestAk123 <ilya_i_2@mail.ru>
2022-12-04 20:41:36 +0700

    Create main.cpp

---
+++ main.cpp
@@ -0,0 +1,26 @@
+﻿#include <iostream>
+using namespace std;
+double my_pow(double number, unsigned int power)
+{
+    double numpow = 1;
+    for (int i = 0; i < power; i++)
+    {
+        numpow *= number;
+    }
+    return numpow;
+}
+int main()
+{
+    setlocale(LC_ALL, "russian");
+    double number;
+    unsigned int power;
+    cout << "Введите дробное число" << endl;
+    cin >> number;
+    cout << "Введите положительную целую степень" << endl;
+    cin >> power;
+    double answer = my_pow(number, power);
+    cout << "Число: " << number << endl;
+    cout << "Степень: " << power << endl;
+    cout << "Число^(Степень): " << answer << endl;
+    return 0;
+}
